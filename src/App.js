e4df9aa59c447aa8d67b61b3b25cb3d6931b8d28
ismailcubuk <iismailcubuk@gmail.com>
2023-01-25 17:04:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [champions, setChampions] = useState([])
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    await fetch('http://ddragon.leagueoflegends.com/cdn/13.1.1/data/en_US/champion.json')
+      .then(response => response.json())
+      .then(json => setChampions(json.data))
+  }
+  const result = (Object.keys(champions).map((key) => champions[key]));
+  console.log(result[0]);
+
+
   return (
     <div>
-      new update
+      {
+        result.map((hero) => {
+          return <div>
+            <div>{hero.id}</div>
+            <div>{hero.title}</div>
+            <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${hero.id}_0.jpg`} width="50px" height="100px" alt="asd" />
+            <div className='info'>
+              <div>Attack = {hero.info.attack}</div>
+              <div>Defense = {hero.info.defense}</div>
+              <div>Magic = {hero.info.magic}</div>
+            </div>
+            <div className='tags'>
+              <div> {hero.tags} </div>
+            </div>
+            <br />
+            <p> {hero.blurb} </p>
+
+            <hr />
+          </div>
+        })
+      }
     </div>
   )
 }
