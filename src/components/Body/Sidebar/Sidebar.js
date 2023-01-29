ad9ff60f9bcde8d4092643cbd44bc4fa310fc798
ismailcubuk <iismailcubuk@gmail.com>
2023-01-29 20:22:35 +0300

    need to price filter

--- src/components/Body/Sidebar/Sidebar.js
+++ src/components/Body/Sidebar/Sidebar.js
@@ -1,18 +1,18 @@
 import { BsSearch } from 'react-icons/bs';
-import { AiOutlineArrowUp, AiOutlineArrowDown } from 'react-icons/ai';
+import { AiOutlineArrowUp, AiOutlineArrowDown, AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import './sidebar.css'
 import { useContext } from 'react';
 import CardContext from '../../CardContext';
 
 function Sidebar() {
-    const { handleChange, allRoleCLick, fighterClick, tankClick, mageClick, assassinClick, marksmanClick, supportClick } = useContext(CardContext)
+    const { handleChange, allRoleCLick, clickedAllRoles, clickedFighter, clickedTank, clickedMage, clickedAssassin, clickedMarksman, clickedSupport, fighterClick, tankClick, mageClick, assassinClick, marksmanClick, supportClick } = useContext(CardContext)
     return (
         <div className='sidebar-border'>
             <div className='sidebar-inside'>
                 <div className='check'>
                     <button className='all-role'>
                         All Role
-                        <div className='all-role-images' onClick={allRoleCLick}>
+                        <div className='all-role-images' onClick={allRoleCLick} style={clickedAllRoles}>
                             <img src="https://raw.communitydragon.org/7.20/plugins/rcp-fe-lol-champion-details/global/default/role-icon-fighter.png" alt="" />
                             <img src="https://raw.communitydragon.org/7.20/plugins/rcp-fe-lol-champion-details/global/default/role-icon-tank.png" alt="" />
                             <img src="https://raw.communitydragon.org/7.20/plugins/rcp-fe-lol-champion-details/global/default/role-icon-mage.png" alt="" />
@@ -25,29 +25,29 @@ function Sidebar() {
                         <div>Role Filter</div>
                     </div>
                     <div className='role-filter-main' >
-                        <button className='role' onClick={fighterClick}>
+                        <button className='role' onClick={fighterClick} style={clickedFighter}>
                             <img src="https://raw.communitydragon.org/7.20/plugins/rcp-fe-lol-champion-details/global/default/role-icon-fighter.png" alt="" />
                             Fighter
                         </button>
 
-                        <button className='role' onClick={tankClick}>
+                        <button className='role' onClick={tankClick} style={clickedTank}>
                             <img src="https://raw.communitydragon.org/7.20/plugins/rcp-fe-lol-champion-details/global/default/role-icon-tank.png" alt="" />
                             Tank
                         </button>
 
-                        <button className='role' onClick={mageClick}>
+                        <button className='role' onClick={mageClick} style={clickedMage}>
                             <img src="https://raw.communitydragon.org/7.20/plugins/rcp-fe-lol-champion-details/global/default/role-icon-mage.png" alt="" />
                             Mage
                         </button>
-                        <button className='role' onClick={assassinClick}>
+                        <button className='role' onClick={assassinClick} style={clickedAssassin} >
                             <img src="https://raw.communitydragon.org/7.20/plugins/rcp-fe-lol-champion-details/global/default/role-icon-assassin.png" alt="" />
                             Assassin
                         </button>
-                        <button className='role' onClick={marksmanClick}>
+                        <button className='role' onClick={marksmanClick} style={clickedMarksman}>
                             <img src="https://raw.communitydragon.org/7.20/plugins/rcp-fe-lol-champion-details/global/default/role-icon-marksman.png" alt="" />
                             Marksman
                         </button>
-                        <button className='role' onClick={supportClick}>
+                        <button className='role' onClick={supportClick} style={clickedSupport}>
                             <img src="https://raw.communitydragon.org/7.20/plugins/rcp-fe-lol-champion-details/global/default/role-icon-support.png" alt="" />
                             Support
                         </button>
@@ -59,9 +59,16 @@ function Sidebar() {
                             <button className='sidebar-button'>
                                 <AiOutlineArrowUp />
                                 <div className='price-text'>
-                                    Price Low to High
+                                    Price High to Low
                                 </div>
                                 <AiOutlineArrowUp />
+                            </button>
+                            <button className='sidebar-button'>
+                                <AiOutlineArrowLeft />
+                                <div className='price-text'>
+                                    Price Low to High
+                                </div>
+                                <AiOutlineArrowRight />
                             </button>
                             <button className='sidebar-button'>
                                 <AiOutlineArrowDown />
--- src/components/CardContext.js
+++ src/components/CardContext.js
@@ -18,30 +18,144 @@ export const CardContextprovider = ({ children }) => {
     const [search, setSearch] = useState("")
     const [datas, setDatas] = useState("")
 
+
     const handleChange = (e) => {
         setSearch(e.target.value)
     }
+    //               + ALLROLES +
+    const [activeAllRoles, setActiveAllRoles] = useState(false)
+    const clickedAllRoles = {
+        marginLeft: activeAllRoles ? "-4px" : "",
+        opacity: activeAllRoles ? 1 : "",
+        borderLeft: activeAllRoles ? "4px solid red" : "",
+        borderTop: activeAllRoles ? "none" : "",
+        paddingLeft: activeAllRoles ? "10px" : ""
+    }
     const allRoleCLick = () => {
         setDatas("")
+        setActiveAllRoles(true)
+        setActiveFighter(false)
+        setActiveTank(false)
+        setActiveMage(false)
+        setActiveAssassin(false)
+        setActiveMarksman(false)
+        setActiveSupport(false)
     }
-    const tankClick = () => {
-        setDatas("Tank")
-    }
-    const mageClick = () => {
-        setDatas("Mage")
-    }
-    const assassinClick = () => {
-        setDatas("Assassin")
-    }
-    const marksmanClick = () => {
-        setDatas("Marksman")
-    }
-    const supportClick = () => {
-        setDatas("Support")
+    //               - ALLROLES -
+    //               + FİGHTER +
+    const [activeFighter, setActiveFighter] = useState(false)
+    const clickedFighter = {
+        marginLeft: activeFighter ? "-4px" : "",
+        opacity: activeFighter ? 1 : "",
+        borderLeft: activeFighter ? "4px solid red" : "",
+        borderTop: activeFighter ? "none" : "",
     }
     const fighterClick = () => {
         setDatas("Fighter")
+        setActiveAllRoles(false)
+        setActiveFighter(true)
+        setActiveTank(false)
+        setActiveMage(false)
+        setActiveAssassin(false)
+        setActiveMarksman(false)
+        setActiveSupport(false)
     }
+    //               - FİGHTER -
+    //                 + TANK +
+    const [activeTank, setActiveTank] = useState(false)
+    const clickedTank = {
+        marginLeft: activeTank ? "-4px" : "",
+        opacity: activeTank ? 1 : "",
+        borderLeft: activeTank ? "4px solid red" : "",
+        borderTop: activeTank ? "none" : "",
+    }
+    const tankClick = () => {
+        setDatas("Tank")
+        setActiveAllRoles(false)
+        setActiveFighter(false)
+        setActiveTank(true)
+        setActiveMage(false)
+        setActiveAssassin(false)
+        setActiveMarksman(false)
+        setActiveSupport(false)
+    }
+    //                 - TANK -
+    //                 + MAGE +
+    const [activeMage, setActiveMage] = useState(false)
+    const clickedMage = {
+        marginLeft: activeMage ? "-4px" : "",
+        opacity: activeMage ? 1 : "",
+        borderLeft: activeMage ? "4px solid red" : "",
+        borderTop: activeMage ? "none" : "",
+    }
+    const mageClick = () => {
+        setDatas("Mage")
+        setActiveAllRoles(false)
+        setActiveFighter(false)
+        setActiveTank(false)
+        setActiveMage(true)
+        setActiveAssassin(false)
+        setActiveMarksman(false)
+        setActiveSupport(false)
+    }
+    //                 - MAGE -
+    //                 + ASSASSİN +
+    const [activeAssassin, setActiveAssassin] = useState(false)
+    const clickedAssassin = {
+        marginLeft: activeAssassin ? "-4px" : "",
+        opacity: activeAssassin ? 1 : "",
+        borderLeft: activeAssassin ? "4px solid red" : "",
+        borderTop: activeAssassin ? "none" : "",
+    }
+    const assassinClick = () => {
+        setDatas("Assassin")
+        setActiveAllRoles(false)
+        setActiveFighter(false)
+        setActiveTank(false)
+        setActiveMage(false)
+        setActiveAssassin(true)
+        setActiveMarksman(false)
+        setActiveSupport(false)
+    }
+    //               - ASSASSİN -
+    //               + MARKSMAN +
+    const [activeMarksman, setActiveMarksman] = useState(false)
+    const clickedMarksman = {
+        marginLeft: activeMarksman ? "-4px" : "",
+        opacity: activeMarksman ? 1 : "",
+        borderLeft: activeMarksman ? "4px solid red" : "",
+        borderTop: activeMarksman ? "none" : "",
+    }
+    const marksmanClick = () => {
+        setDatas("Marksman")
+        setActiveAllRoles(false)
+        setActiveFighter(false)
+        setActiveTank(false)
+        setActiveMage(false)
+        setActiveAssassin(false)
+        setActiveMarksman(true)
+        setActiveSupport(false)
+    }
+    //               - MARKSMAN -
+    //               + SUPPORT +
+    const [activeSupport, setActiveSupport] = useState(false)
+    const clickedSupport = {
+        marginLeft: activeSupport ? "-4px" : "",
+        opacity: activeSupport ? 1 : "",
+        borderLeft: activeSupport ? "4px solid red" : "",
+        borderTop: activeSupport ? "none" : "",
+    }
+    const supportClick = () => {
+        setDatas("Support")
+        setActiveAllRoles(false)
+        setActiveFighter(false)
+        setActiveTank(false)
+        setActiveMage(false)
+        setActiveAssassin(false)
+        setActiveMarksman(false)
+        setActiveSupport(true)
+    }
+    //               - SUPPORT -
 
     const result = (Object.keys(champions).map((key) => champions[key]));
     const filtered = result.filter(filtered =>
@@ -54,12 +168,19 @@ export const CardContextprovider = ({ children }) => {
 
     // .slice(0, 20)
     const filteredChamp = filtered.filter(filteredText => filteredText.id.toLowerCase().includes(search))
-    const newArray = filtered.filter(item => item.tags.some(tags => tags.includes(datas))).map(obj => {
+    const newArray = filteredChamp.filter(item => item.tags.some(tags => tags.includes(datas))).map(obj => {
         return obj;
     });
 
     const data = {
         newArray,
+        clickedAllRoles,
+        clickedFighter,
+        clickedTank,
+        clickedMage,
+        clickedAssassin,
+        clickedMarksman,
+        clickedSupport,
         handleChange,
         allRoleCLick,
         fighterClick,
